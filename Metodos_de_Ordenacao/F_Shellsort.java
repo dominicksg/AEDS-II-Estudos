0aebb034f9e2764b0f09b8905742884fd003d6be
dominicsg <douglasnicsg@hotmail.com>
2024-05-02 15:15:33 -0300

    #

--- Metodos_de_Ordenacao/F_Shellsort.java
+++ Metodos_de_Ordenacao/F_Shellsort.java
@@ -4,17 +4,37 @@ class F_Shellsort {
     static void shellsort(int[] array) {
         int[] counts = { 0, 0 }; // counts[0] para comparações, counts[1] para movimentações
         int h = 1;
-        do {
+        while (h < array.length) {
             h = (h * 3) + 1;
-        } while (h < array.length);
-        do {
+        }
+        while (h != 1) {
             h /= 3;
             for (int cor = 0; cor < h; cor++) {
                 insercaoPorCor(cor, h, array, counts);
             }
-        } while (h != 1);
+        }
         System.out.println("Foram feitas " + counts[0] + " comparacoes e " + counts[1] + " movimentacoes.");
     }
+
+    // O código abaixo é o original, e é mais eficiente também,
+    // pois não precisa verificar a condição antes da primeira execução
+    // do bloco de código.
+    // Mesmo assim, montei o código anterior para melhor entendimento e estudo.
+    // int[] counts = { 0, 0 }; // counts[0] para comparações, counts[1] para
+    // movimentações
+    // int h = 1;
+    // do {
+    // h = (h * 3) + 1;
+    // } while (h < array.length);
+    // do {
+    // h /= 3;
+    // for (int cor = 0; cor < h; cor++) {
+    // insercaoPorCor(cor, h, array, counts);
+    // }
+    // } while (h != 1);
+    // System.out.println("Foram feitas " + counts[0] + " comparacoes e " +
+    // counts[1] + " movimentacoes.");
+    // }
 
     static void insercaoPorCor(int cor, int h, int[] array, int[] counts) {
         for (int i = (h + cor); i < array.length; i += h) {
