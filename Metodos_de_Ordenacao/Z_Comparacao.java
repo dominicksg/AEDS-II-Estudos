375a49a3bc9b0c4b8316fa452e3b2887fbd67493
dominicsg <douglasnicsg@hotmail.com>
2024-05-12 15:33:19 -0300

    #

--- Metodos_de_Ordenacao/Z_Comparacao.java
+++ Metodos_de_Ordenacao/Z_Comparacao.java
@@ -5,6 +5,7 @@ class Z_Comparacao {
     public static void main(String[] args) {
         Scanner Sc = new Scanner(System.in);
         // Terá a comparação de todos os metodos aqui.
+        Sc.close();
 
     }
 }
